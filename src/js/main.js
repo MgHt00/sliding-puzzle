321d4e744c1345df2638df602599dc95e045d43c
Mg Htoo <whshwe@gmail.com>
2025-08-09 12:54:11 +0630

    DEVELOP: UI: made winning alert similar with the confirmation alert

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import '../lib/bootstrap.bundle.js';
 import { startGame } from './components/loadingManager.js'; // eslint-disable-line no-unused-vars
-import { showLoadingScreen, hideLoadingScreen, showConfirmationScreen } from './utils/domHelpers.js'; // eslint-disable-line no-unused-vars
+import { showLoadingScreen, hideLoadingScreen, showWinningScreen, showConfirmationScreen } from './utils/domHelpers.js'; // eslint-disable-line no-unused-vars
 
 /**
  * Main function to initialize the puzzle.
